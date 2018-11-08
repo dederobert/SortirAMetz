988feb072c6bdf8e2838cdd406dde805350f7b92
dederobert <adinquer@yahoo.com>
2018-11-08 19:08:08 +0100

    Initial commit

---
+++ app/src/main/java/a1819/m2ihm/sortirametz/ActivityClientMap.java
@@ -0,0 +1,13 @@
+package a1819.m2ihm.sortirametz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityClientMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_map);
+    }
+}
