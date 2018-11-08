1d5bcfd0997a60833c868e48dbf51b47fa782ae8
dederobert <adinquer@yahoo.com>
2018-11-08 19:46:49 +0100

    Interface

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sortir A Metz
+
+Cette application Android permet de trouver des lieux tourristiques à visiter à Metz
--- app/src/main/java/a1819/m2ihm/sortirametz/ActivityClientMap.java
+++ app/src/main/java/a1819/m2ihm/sortirametz/ActivityClientMap.java
@@ -2,6 +2,9 @@ package a1819.m2ihm.sortirametz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ActivityClientMap extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class ActivityClientMap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_map);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Permet d'ajouter un menu en haut à droite
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //gestion des cliques dans le menu
+        return super.onOptionsItemSelected(item);
     }
 }
